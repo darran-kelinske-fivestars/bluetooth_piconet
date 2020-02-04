659a7006fd58db60e07f4e4ab676b508a135f9a4
Darran Kelinske <darran.kelinske@gmail.com>
2020-02-04 11:09:06 -0600

    Update dependencies

--- app/src/main/java/ilteoood/bluetoothpiconet/MainActivity.java
+++ app/src/main/java/ilteoood/bluetoothpiconet/MainActivity.java
@@ -8,7 +8,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
